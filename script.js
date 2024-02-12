344ffafd4d8709fbac0e6749b300bb41a12c9b98
indakwa <indakwabenedict@gmail.com>
2024-02-12 14:59:33 +0300

    1 comm

---
+++ script.js
@@ -0,0 +1,4 @@
+let blockElement = document.getElementById('gallery');
+blockElement.addEventListener('click', function() {
+    window.location.href = 'https://google.com'; // Replace 'https://example.com' with your desired URL
+});
